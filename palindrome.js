4266f1fff4ff942e025b90df20c63b2be55d6ab3
Ashish <erashish2k16@gmail.com>
2022-11-02 23:27:07 +0530

    created prime and palindrome files

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+
+N = 5;
+
+str = "Vijay";
+
+let bag = "";
+
+for (i = N - 1; i >= 0; i--) {
+
+  bag = bag + str[i];
+
+} if (bag == str) {
+
+  console.log("Yes);
+} else {
+
+  console.log("No);
+}
---
+++ prime.js
@@ -0,0 +1,17 @@
+num = 13;
+
+count = 0;
+
+for (i = 1; i <= num; i++) {
+
+  if (num % i == 0) {
+
+    count++
+  }
+}
+if (count == 2) {
+  console.log("Yes");
+} else {
+
+  console.log("No");
+}
